efae3c3676c826bb721f07c30829b783b00cbd46
Prasanna Natarajan <prasann87@gmail.com>
2015-07-20 20:42:10 +0530

    executables FTW. With TODO

--- lib/path.sh
+++ lib/path.sh
@@ -1,9 +1,13 @@
 export PATH=$PATH:~/bin
 
+# re
+export PATH=$PATH:~/bin/cnp_helpers/bin
+
 # RVM
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+export PATH="$PATH:$HOME/.rvm/bin"
 
 if [ $(uname) == 'Darwin' ]
 then
   export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
 fi
+
