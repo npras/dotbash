56de4605f0c37997b235a3ede9395dda65360e6d
Prasanna.Natarajan <prasanna@npras.in>
2016-03-10 17:24:11 +0530

    no more rvm to manage rubies. Yay\!

--- lib/path.sh
+++ lib/path.sh
@@ -3,9 +3,6 @@ export PATH=$PATH:~/bin
 # re
 export PATH=$PATH:~/bin/cnp_helpers/bin
 
-# RVM
-export PATH="$PATH:$HOME/.rvm/bin"
-
 if [ $(uname) == 'Darwin' ]
 then
   export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
