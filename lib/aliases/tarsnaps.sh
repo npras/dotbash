e6512a222e6384fd02bbab9e452bb8d6406bbae4
Prasanna.Natarajan <prasanna@npras.in>
2016-03-12 16:04:22 +0530

    tsnap aliases

--- lib/aliases/tarsnaps.sh
+++ lib/aliases/tarsnaps.sh
@@ -1,12 +1,11 @@
 alias tshelp="echo TARSNAP aliases: &&
           echo ================ &&
-          echo tslist   - (sudo tarsnap --list-archives | sort) - list the archived backups &&
-          echo tsstats - (sudo tarsnap --print-stats -f '*') - stats (size) of all backups &&
-          echo tsbackup - (sudo /root/tarsnap-backup.sh) - backup files in /opt/backup_here &&
-          echo tsrestore - (sudo tarsnap -x -f <backup-name>) - restore any of the archives &&
-          echo tsdelete - (sudo tarsnap -d -f <archive-name>) - delete a backup archive from cloud &&
-          echo tsdryrun - (sudo tarsnap --dry-run -v --no-default-config --print-stats --humanize-numbers -c <path-to-backup>) - find how much size the compressed backup of a folder would be if we create a backup &&
-          "
+          echo tslist   - list the archived backups &&
+          echo tsstats - stats and size of all backups &&
+          echo tsbackup - backup files in /opt/backup_here &&
+          echo tsrestore - restore any of the archives &&
+          echo tsdelete - delete a backup archive from cloud &&
+          echo tsdryrun - find how much size the compressed backup of a folder would be if we create a backup"
 
 
 alias tslist='sudo tarsnap --list-archives | sort'
