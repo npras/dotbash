0397176e51921ec470446503cd41b1cef82c73df
Prasanna.Natarajan <prasanna.natarajan@velankani.com>
2016-05-02 11:15:16 +0530

    xmodemap not needed anymore

--- lib/xmodmap.sh
+++
@@ -1,8 +0,0 @@
-# Script to change caps lock to control
-
-if [ $(uname) == 'Linux' ]
-then
-  xmodmap -e 'remove Lock = Caps_Lock'
-  xmodmap -e 'keysym Caps_Lock = Control_L'
-  xmodmap -e 'add Control = Control_L'
-fi
